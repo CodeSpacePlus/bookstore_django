757aaa312b3e6695e995d01ed94b809d533fcd6f
CodeSpacePlus <bryanc0791@gmail.com>
2020-06-09 12:55:41 -0700

    Created unit tests for pages app.

--- bookstore_project/settings.py
+++ bookstore_project/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users.apps.UsersConfig',
-    'pages.apps.AppConfig',
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
